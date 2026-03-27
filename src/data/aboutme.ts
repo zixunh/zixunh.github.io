1daab31c464d67b2781175c2731731a81ecf3cea
zxxxxh <106426767+zixunh@users.noreply.github.com>
2026-03-26 22:37:11 -0700

    Update title from 'Research Scientist' to 'Senior Research Scientist'

--- src/data/aboutme.ts
+++ src/data/aboutme.ts
@@ -19,7 +19,7 @@ export interface AboutMe {
 
 export const aboutMe: AboutMe = {
   name: "Zixun Huang",
-  title: "Research Scientist",
+  title: "Senior Research Scientist",
   institution: "Bosch | UC Berkeley",
   // Note that links work in the description
   description: "Good representations arise from simple underlying structure.",
