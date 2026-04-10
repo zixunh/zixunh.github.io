521c74283914a2d2fd86e425b743329c8561056b
zixun <zixun@berkeley.edu>
2026-04-09 17:23:21 -0700

    update self intro

--- next.config.ts
+++ next.config.ts
@@ -6,11 +6,7 @@ const nextConfig: NextConfig = {
       {
         protocol: "https",
         hostname: "images.unsplash.com",
-      },
-      {
-        protocol: "https",
-        hostname: "69a8c69562aea864bacd7cca.imgix.net",
-      },
+      }
     ],
   },
 };
