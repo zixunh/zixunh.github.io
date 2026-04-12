e394c6d4810d08ebcba5e918e93ad88bcc4943c5
zixun <zixun@berkeley.edu>
2026-04-12 02:52:23 -0700

    update self intro

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -45,31 +45,6 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} ${notoSerif.variable} ${ptSerif.variable} antialiased`}
       >
         <main className="">{children}</main>
-        {/* <footer className="border-t border-neutral-200 dark:border-neutral-800 bg-[#FFFCF8]">
-          <div className="flex flex-row mx-auto max-w-7xl px-6 py-12 md:flex md:items-start md:justify-between ">
-            <div className="mb-4 text-sm text-neutral-600 dark:text-neutral-400">
-              <p>
-                  © {currentYear} {aboutMe.name}.
-              </p>
-              {aboutMe.secretDescription && (
-                <p className="text-xs text-neutral-600 dark:text-neutral-400 mt-4">
-                  {aboutMe.secretDescription}
-                </p>
-              )}
-            </div>
-            <div className="mb-4">
-              <p className="text-sm text-neutral-500 dark:text-neutral-500 justify">
-                Built with{" "}
-                <a
-                  href="https://github.com/tovacinni/research-website-template"
-                  className="underline hover:text-neutral-800 dark:hover:text-neutral-300 transition-colors"
-                >
-                  research-website-template
-                </a>
-              </p>
-            </div>
-          </div>
-        </footer> */}
         <footer className="border-t border-neutral-200 dark:border-neutral-800 bg-[#FFFCF8]">
           <div className="flex flex-col md:flex-row mx-auto max-w-7xl px-6 py-12 md:items-start md:justify-between">
             <div className="mb-4 text-sm text-neutral-600 dark:text-neutral-400">
